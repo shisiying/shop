85ef9e22875bfa148b0cc6c6c867062decfcea45
vagrant <vagrant@homestead>
2018-10-30 01:34:54 +0000

    添加horizon

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="{{ url('/') }}">
-                Laravel Shop
+                Shop
             </a>
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
+    <title>@yield('title', 'Laravel Shop') - Shop</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
